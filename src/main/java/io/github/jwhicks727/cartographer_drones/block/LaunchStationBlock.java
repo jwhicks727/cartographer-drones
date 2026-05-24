d03e8035655aa552d9e77588849d72ca1494db76
jwhicks727 <jwhicks727@gmail.com>
2026-05-24 11:10:59 -0700

    feat: add facing blockstate to LaunchStationBlock, add block model and blockstate JSON

--- src/main/java/io/github/jwhicks727/cartographer_drones/block/LaunchStationBlock.java
+++ src/main/java/io/github/jwhicks727/cartographer_drones/block/LaunchStationBlock.java
@@ -2,22 +2,48 @@ package io.github.jwhicks727.cartographer_drones.block;
 
 import io.github.jwhicks727.cartographer_drones.block.entity.LaunchStationBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.core.Direction;
 import net.minecraft.world.phys.BlockHitResult;
 import com.mojang.serialization.MapCodec;
 
-
 public class LaunchStationBlock extends BaseEntityBlock {
+
+    public static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
 
     public LaunchStationBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any()
+            .setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return simpleCodec(LaunchStationBlock::new);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING,
+            context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
@@ -41,9 +67,4 @@ public class LaunchStationBlock extends BaseEntityBlock {
         }
         return InteractionResult.SUCCESS;
     }
-
-    @Override
-    protected MapCodec<? extends BaseEntityBlock> codec() {
-    return simpleCodec(LaunchStationBlock::new);
-}
 }
